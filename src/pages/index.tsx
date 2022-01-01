f11eff07a847c0893a3bfb9a6a398ea62b7076a7
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2022-01-01 17:21:41 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KDDHWJF7tRLMTcn4JCwOipI");
   const product = {
     priceOd: price.id,
@@ -48,5 +48,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 1 day
   };
 };
