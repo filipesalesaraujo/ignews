27f55c27120e4055288785b4bc91ef81122c09c2
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2022-01-01 14:59:09 -0300

    Estilos globais do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import Head from "next/head";
-import styles from "../styles/home.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello <span>Next.js</span>
-      </h1>
+      <h1>Hello Next.js</h1>
     </>
   );
 }
