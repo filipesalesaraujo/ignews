cc1f77a131ac87dcb42ae858b8c049287f8b5be2
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2022-01-03 19:52:02 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,19 @@
-import Head from "next/head";
-import { getPrismicCliente } from "../../services/prismc";
-import styles from "./styles.module.scss";
-import Prismic from "@prismicio/client";
 import { GetStaticProps } from "next";
-
-export default function Posts() {
+import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import styles from "./styles.module.scss";
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,55 +21,45 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>
-              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
-            </strong>
-            <p>
-              Morbi ut volutpat urna, sit amet vulputate risus. Duis at cursus
-              justo, nec faucibus mi. Curabitur mi ipsum, semper non
-              pellentesque id, lacinia ac magna.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>
-              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
-            </strong>
-            <p>
-              Morbi ut volutpat urna, sit amet vulputate risus. Duis at cursus
-              justo, nec faucibus mi. Curabitur mi ipsum, semper non
-              pellentesque id, lacinia ac magna.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>
-              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
-            </strong>
-            <p>
-              Morbi ut volutpat urna, sit amet vulputate risus. Duis at cursus
-              justo, nec faucibus mi. Curabitur mi ipsum, semper non
-              pellentesque id, lacinia ac magna.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicCliente();
+  const prismic = getPrismicClient();
   const response = await prismic.query(
     [Prismic.predicates.at("document.type", "posts")],
     {
-      fetch: ["publication.title", "publication.content"],
+      fetch: ["posts.title", "posts.content"],
       pageSize: 100,
     }
   );
-  console.log(response);
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last.posts_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
   return {
-    props: {},
+    props: {posts},
   };
 };
