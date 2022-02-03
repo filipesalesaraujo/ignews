301d975edc6f6f7c6671d0bb4d2792513effa689
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-03 17:58:55 -0300

    Testando primeiro componente

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import { useRouter } from "next/router";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+describe("ActiveLink component", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  it("adds active class if the link as currently active", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toHaveClass("active");
+  });
+});
